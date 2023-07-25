723c3ede35704a17d051a124271ef1a1e12a9d57
Alec Gunny <alec.gunny@gmail.com>
2023-07-25 16:49:12 -0700

    working out highpass normalization issues in local whitening

--- tests/test_spectral.py
+++ tests/test_spectral.py
@@ -410,7 +410,16 @@ def test_whiten(
 
     stds = whitened.std(axis=-1)
     target = torch.ones_like(stds)
-    torch.testing.assert_close(stds, target, rtol=0, atol=0.07)
+
+    # if we're highpassing, then we shouldn't expect
+    # the standard deviation to be one because we're
+    # subtracting some power, so remove roughly the
+    # expected power contributed by the highpassed
+    # frequencies from the target
+    if highpass is not None:
+        nyquist = sample_rate / 2
+        target *= (1 - highpass / nyquist) ** 0.5
+    torch.testing.assert_close(stds, target, rtol=0.05, atol=0.0)
 
     # check that frequencies up to close to the highpass
     # frequency have near 0 power
@@ -419,7 +428,7 @@ def test_whiten(
         idx = int(0.8 * highpass * whiten_length)
         passed = fft[:, :, :idx]
         target = torch.zeros_like(passed)
-        torch.testing.assert_close(passed, target, rtol=0, atol=0.05)
+        torch.testing.assert_close(passed, target, rtol=0, atol=0.07)
 
     # inject a gaussian pulse into the timeseries and
     # ensure that its max value comes out to the same place
