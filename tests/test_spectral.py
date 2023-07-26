f25ba2d4fa22485e5cae33ccd41953280cf4eb42
Alec Gunny <alec.gunny@gmail.com>
2023-07-26 11:13:02 -0700

    completed FixedWhiten tests

--- tests/test_spectral.py
+++ tests/test_spectral.py
@@ -362,6 +362,7 @@ def test_whiten(
     ndim,
     whiten_length,
     background_length,
+    validate_whitened,
 ):
     batch_size = 8
     num_channels = 5
@@ -403,32 +404,7 @@ def test_whiten(
     expected_size = int((whiten_length - fduration) * sample_rate)
     assert whitened.shape == (batch_size, num_channels, expected_size)
 
-    # make sure we have 0 mean unit variance
-    means = whitened.mean(axis=-1)
-    target = torch.zeros_like(means)
-    torch.testing.assert_close(means, target, rtol=0, atol=0.02)
-
-    stds = whitened.std(axis=-1)
-    target = torch.ones_like(stds)
-
-    # if we're highpassing, then we shouldn't expect
-    # the standard deviation to be one because we're
-    # subtracting some power, so remove roughly the
-    # expected power contributed by the highpassed
-    # frequencies from the target
-    if highpass is not None:
-        nyquist = sample_rate / 2
-        target *= (1 - highpass / nyquist) ** 0.5
-    torch.testing.assert_close(stds, target, rtol=0.05, atol=0.0)
-
-    # check that frequencies up to close to the highpass
-    # frequency have near 0 power
-    if highpass is not None:
-        fft = torch.fft.rfft(whitened, norm="ortho").abs()
-        idx = int(0.8 * highpass * whiten_length)
-        passed = fft[:, :, :idx]
-        target = torch.zeros_like(passed)
-        torch.testing.assert_close(passed, target, rtol=0, atol=0.07)
+    validate_whitened(whitened, highpass, sample_rate, 1 / whiten_length)
 
     # inject a gaussian pulse into the timeseries and
     # ensure that its max value comes out to the same place
--- tests/transforms/test_whitening.py
+++ tests/transforms/test_whitening.py
@@ -1,9 +1,12 @@
+from unittest.mock import patch
+
 import numpy as np
 import pytest
 import torch
 from gwpy.timeseries import TimeSeries
 
-from ml4gw.transforms import Whiten
+from ml4gw.spectral import spectral_density
+from ml4gw.transforms import FixedWhiten, Whiten
 
 
 @pytest.fixture(params=[512, 1024])
@@ -149,3 +152,141 @@ def test_whitening_save_and_load(dtype, tmp_path):
     torch.save(nn.state_dict(), tmp_path / "weights.pt")
     nn.load_state_dict(torch.load(tmp_path / "weights.pt"))
     assert tform.built
+
+
+class TestFixedWhiten:
+    sample_rate = 256
+    psd_length = 128
+    whiten_length = 64
+    num_channels = 5
+
+    mean = 2
+    std = 5
+
+    def get_transform(self):
+        return FixedWhiten(
+            self.num_channels, self.whiten_length, self.sample_rate
+        )
+
+    @pytest.fixture
+    def transform(self):
+        return self.get_transform()
+
+    @pytest.fixture
+    def background(self):
+        background_size = int(self.psd_length * self.sample_rate)
+        x = [torch.randn(background_size) for _ in range(self.num_channels)]
+        return [self.mean + self.std * i for i in x]
+
+    @pytest.fixture
+    def X(self):
+        size = int(self.whiten_length * self.sample_rate)
+        X = torch.randn(8, self.num_channels, size)
+        return self.mean + self.std * X
+
+    @pytest.fixture(params=[None, 32])
+    def highpass(self, request):
+        return request.param
+
+    def test_init(self, transform, X):
+        # ensure parameters have been initialized
+        # with the right shapes and to 0 valuess
+        assert transform.psd.shape == (self.num_channels, 8193)
+        assert (transform.psd == 0).all().item()
+
+        assert transform.fduration.shape == (1,)
+        assert (transform.fduration == 0).item()
+
+        # ensure that trying to call forward
+        # before fitting raises an error
+        assert not transform.built
+        with pytest.raises(ValueError) as exc:
+            transform(X)
+        assert str(exc.value).startswith("Must fit parameters")
+
+    def test_fit_time_domain(
+        self, transform, background, X, highpass, validate_whitened
+    ):
+        # ensure that calling with the wrong number
+        # of background channels raises an error
+        with pytest.raises(ValueError) as exc:
+            transform.fit(2, *background[:-1], fftlength=4)
+        assert str(exc.value).startswith("Expected to fit whitening")
+
+        # fit to the background and ensure that the
+        # parameter values have been set to nonzero values
+        transform.fit(2, *background, fftlength=4, highpass=highpass)
+        assert (transform.psd != 0).all().item()
+        assert (transform.fduration == 2).item()
+
+        # now whiten a dummy tensor and make sure
+        # that the values come out as expected
+        whitened = transform(X)
+        validate_whitened(
+            whitened, highpass, self.sample_rate, 1 / self.whiten_length
+        )
+
+    def get_psds(self, background, fftlength):
+        nperseg = int(fftlength * self.sample_rate)
+        window = torch.hann_window(nperseg)
+        psds = []
+        for bg in background:
+            psd = spectral_density(
+                bg,
+                nperseg,
+                nperseg // 2,
+                window,
+                scale=1 / (self.sample_rate * (window**2).sum()),
+            )
+            psds.append(psd)
+        return psds
+
+    def test_fit_freq_domain(
+        self, transform, background, X, highpass, validate_whitened
+    ):
+        # first check if fftlength == self.whiten_length,
+        # the fit psd should match the psds used to fit
+        # almost exactly if we don't use inverse spectrum
+        # truncation
+        psds = self.get_psds(background, self.whiten_length)
+        with patch(
+            "ml4gw.transforms.whitening"
+            ".spectral.truncate_inverse_power_spectrum",
+            new=lambda x, _, __, ___: x,
+        ):
+            transform.fit(2, *psds)
+        assert (transform.fduration == 2).item()
+        for i, psd in enumerate(psds):
+            torch.testing.assert_close(
+                psd, transform.psd[i], rtol=1e-6, atol=0.0
+            )
+
+        # now do a fit with a more realistic
+        # fftlength, fduration, and highpass
+        psds = self.get_psds(background, 4)
+        transform.fit(2, *psds, highpass=highpass)
+        assert (transform.psd != 0).all().item()
+        assert (transform.fduration == 2).item()
+
+        # now whiten a dummy tensor and make sure
+        # that the values come out as expected
+        whitened = transform(X)
+        validate_whitened(
+            whitened, highpass, self.sample_rate, 1 / self.whiten_length
+        )
+
+    def test_io(self, transform, background, tmp_path):
+        transform.fit(2, *background, fftlength=4)
+        assert transform.built
+        tmp_path.mkdir(exist_ok=True, parents=True)
+        torch.save(transform.state_dict(), tmp_path / "whiten.pt")
+
+        fresh = self.get_transform()
+        assert not fresh.built
+        assert (fresh.fduration == 0).item()
+        state_dict = torch.load(tmp_path / "whiten.pt")
+        fresh.load_state_dict(state_dict)
+        assert fresh.built
+
+        assert (fresh.fduration == 2).item()
+        assert (fresh.psd == transform.psd).all().item()
