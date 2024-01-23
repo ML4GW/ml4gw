f20e13d086cd9095ca36160958e55348897652b9
William Benoit <benoi090@umn.edu>
2024-01-23 12:02:28 -0600

    Finished docstring and made spectrograms more general

--- ml4gw/transforms/spectrogram.py
+++ ml4gw/transforms/spectrogram.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import torch
 import torch.nn.functional as F
 from torchaudio.transforms import Spectrogram
@@ -5,51 +7,67 @@ from torchaudio.transforms import Spectrogram
 
 class MultiResolutionSpectrogram(torch.nn.Module):
     """
-    Create a single spectrogram that combines information
-    from multiple spectrograms of the same timeseries.
-    Input is expected to have the shape `(B, C, T)`,
-    where `B` is the number of batches, `C` is the number
-    of channels, and `T` is the number of time samples.
+    Create a batch of multi-resolution spectrograms
+    from a batch of timeseries. Input is expected to
+    have the shape `(B, C, T)`, where `B` is the number
+    of batches, `C` is the number of channels, and `T`
+    is the number of time samples.
 
-    Given a list of `n_fft`s, calculate the spectrogram
-    corresponding to each and combine them by taking the
-    maximum value from each bin, which has been normalized.
+    For each timeseries, calculate multiple normalized
+    spectrograms based on the `Spectrogram` `kwargs` given.
+    Combine the spectrograms by taking the maximum value
+    from the nearest time-frequncy bin.
 
     If the largest number of time bins among the spectrograms
     is `N` and the largest number of frequency bins is `M`,
     the output will have dimensions `(B, C, M, N)`
+
+    Args:
+        kernel_length:
+            The length in seconds of the time dimension
+            of the tensor that will be turned into a
+            spectrogram
+        sample_rate:
+            The sample rate of the timeseries in Hz
+        kwargs:
+            Arguments passed in kwargs will used to create
+            `torchaudio.transforms.Spectrogram`s. Each
+            argument should be a list of values. Any list
+            of length greater than 1 should be the same
+            length
     """
 
     def __init__(
-        self,
-        n_ffts: torch.Tensor,
-        sample_rate: float,
-        kernel_length: float,
+        self, kernel_length: float, sample_rate: float, **kwargs
     ) -> None:
         super().__init__()
+        # This method of combination makes sense only when
+        # the spectrograms are normalized, so enforce this
+        kwargs["normalized"] = [True]
+        self.kwargs = self._check_and_format_kwargs(kwargs)
+
         self.transforms = torch.nn.ModuleList(
-            [Spectrogram(n_fft, normalized=True) for n_fft in n_ffts]
+            [Spectrogram(**k) for k in self.kwargs]
         )
 
-        dummy_input = torch.ones(kernel_length * sample_rate)
-        dummy_shapes = torch.tensor(
+        dummy_input = torch.ones(int(kernel_length * sample_rate))
+        self.shapes = torch.tensor(
             [t(dummy_input).shape for t in self.transforms]
         )
-        self.register_buffer("shapes", dummy_shapes)
 
-        self.num_freqs = max([shape[0] for shape in dummy_shapes])
-        self.num_times = max([shape[1] for shape in dummy_shapes])
+        self.num_freqs = max([shape[0] for shape in self.shapes])
+        self.num_times = max([shape[1] for shape in self.shapes])
 
         freq_idxs = torch.tensor(
             [
-                [int(i * shape[0] / self.num_freqs) for shape in dummy_shapes]
+                [int(i * shape[0] / self.num_freqs) for shape in self.shapes]
                 for i in range(self.num_freqs)
             ]
         )
         freq_idxs = freq_idxs.repeat(self.num_times, 1, 1).transpose(0, 1)
         time_idxs = torch.tensor(
             [
-                [int(i * shape[1] / self.num_times) for shape in dummy_shapes]
+                [int(i * shape[1] / self.num_times) for shape in self.shapes]
                 for i in range(self.num_times)
             ]
         )
@@ -58,7 +76,34 @@ class MultiResolutionSpectrogram(torch.nn.Module):
         self.register_buffer("freq_idxs", freq_idxs)
         self.register_buffer("time_idxs", time_idxs)
 
+    def _check_and_format_kwargs(self, kwargs: Dict[str, List]) -> List:
+        lengths = sorted(set([len(v) for v in kwargs.values()]))
+        if len(lengths) > 2 or (len(lengths) == 2 and lengths[0] != 1):
+            raise ValueError(
+                "Spectrogram keyword args should all have the same "
+                f"length or be of length one. Got lengths {lengths}"
+            )
+
+        if len(lengths) == 2:
+            size = lengths[1]
+            kwargs = {k: v * int(size / len(v)) for k, v in kwargs.items()}
+
+        return [dict(zip(kwargs, col)) for col in zip(*kwargs.values())]
+
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """
+        Calculate spectrograms of the input tensor and
+        combine them into a single spectrogram
+
+        Args:
+            X:
+                Batch of multichannel timeseries which will
+                be used to calculate the multi-resolution
+                spectrogram. Should have the shape
+                `(B, C, T)`, where `B` is the number of
+                batches, `C` is the  number of channels,
+                and `T` is the number of time samples.
+        """
         spectrograms = [t(X) for t in self.transforms]
 
         left = 0
