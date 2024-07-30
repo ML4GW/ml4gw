7994bd32eb31af89d36efa7ae2bf9593ecf44a33
William Benoit <william.benoit@ldas-pcdev5.ligo-wa.caltech.edu>
2024-07-30 15:12:15 -0700

    Began switching to jaxtyping

--- ml4gw/types.py
+++ ml4gw/types.py
@@ -1,10 +1,10 @@
-from torchtyping import TensorType
+from jaxtyping import Array, Float
 
-WaveformTensor = TensorType["batch", "num_ifos", "time"]
-PSDTensor = TensorType["num_ifos", "frequency"]
-ScalarTensor = TensorType["batch"]
-VectorGeometry = TensorType["batch", "space"]
-TensorGeometry = TensorType["batch", "space", "space"]
-NetworkVertices = TensorType["num_ifos", 3]
-NetworkDetectorTensors = TensorType["num_ifos", 3, 3]
-TimeSeriesTensor = TensorType["num_channels", "time"]
+WaveformTensor = Float[Array, "batch num_ifos time"]
+PSDTensor = Float[Array, "num_ifos frequency"]
+ScalarTensor = Float[Array, "batch"]
+VectorGeometry = Float[Array, "batch space"]
+TensorGeometry = Float[Array, "batch space space"]
+NetworkVertices = Float[Array, "num_ifos 3"]
+NetworkDetectorTensors = Float[Array, "num_ifos 3 3"]
+TimeSeriesTensor = Float[Array, "num_channels time"]
