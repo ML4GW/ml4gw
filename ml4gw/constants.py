94f0c08e23a4aeba2c1c1aa8094e9f81a815190f
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-12-11 17:42:02 -0800

    add precessing spin conversion

--- ml4gw/constants.py
+++ ml4gw/constants.py
@@ -4,42 +4,33 @@ Various constants, all in SI units.
 
 EulerGamma = 0.577215664901532860606512090082402431
 
+# solar mass
 MSUN = 1.988409902147041637325262574352366540e30  # kg
-"""Solar mass"""
 
+# Geometrized nominal solar mass, m
 MRSUN = 1.476625038050124729627979840144936351e3
-"""Geometrized nominal solar mass, m"""
 
+# Newton's gravitational constant
 G = 6.67430e-11  # m^3 / kg / s^2
-"""Newton's gravitational constant"""
 
+# Speed of light
 C = 299792458.0  # m / s
-"""Speed of light"""
 
-"""Pi"""
+# pi and 2pi
 PI = 3.141592653589793238462643383279502884
-
 TWO_PI = 6.283185307179586476925286766559005768
 
+# G MSUN / C^3 in seconds
 gt = G * MSUN / (C**3.0)
-"""
-G MSUN / C^3 in seconds
-"""
 
+# 1 solar mass in seconds. Same value as lal.MTSUN_SI
 MTSUN_SI = 4.925490947641266978197229498498379006e-6
-"""1 solar mass in seconds. Same value as lal.MTSUN_SI"""
 
+# Meters per Mpc.
 m_per_Mpc = 3.085677581491367278913937957796471611e22
-"""
-Meters per Mpc.
-"""
 
+# 1 Mpc in seconds.
 MPC_SEC = m_per_Mpc / C
-"""
-1 Mpc in seconds.
-"""
 
+# Speed of light in vacuum (:math:`c`), in gigaparsecs per second
 clightGpc = C / 3.0856778570831e22
-"""
-Speed of light in vacuum (:math:`c`), in gigaparsecs per second
-"""
---
+++ tests/conversion.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+from lalsimulation import SimInspiralTransformPrecessingNewInitialConditions
+from torch.distributions import Uniform
+
+from ml4gw.constants import MSUN
+from ml4gw.conversion import bilby_spins_to_lalsim
+
+
+def test_bilby_to_lalsim_spins():
+    thetajn = Uniform(0, torch.pi).sample((100,))
+    phijl = Uniform(0, torch.pi).sample((100,))
+    theta1 = Uniform(0, torch.pi).sample((100,))
+    theta2 = Uniform(0, torch.pi).sample((100,))
+    phi12 = Uniform(0, torch.pi).sample((100,))
+    chi1 = Uniform(0, 0.99).sample((100,))
+    chi2 = Uniform(0, 0.99).sample((100,))
+    mass_1 = Uniform(5, 100).sample((100,))
+    mass_2 = Uniform(5, 100).sample((100,))
+    f_ref = 40.0
+    phi_ref = Uniform(0, torch.pi).sample((100,))
+    incl, s1x, s1y, s1z, s2x, s2y, s2z = bilby_spins_to_lalsim(
+        thetajn,
+        phijl,
+        theta1,
+        theta2,
+        phi12,
+        chi1,
+        chi2,
+        mass_1,
+        mass_2,
+        f_ref,
+        phi_ref,
+    )
+    for i in range(2):
+
+        (
+            lal_incl,
+            lal_s1x,
+            lal_s1y,
+            lal_s1z,
+            lal_s2x,
+            lal_s2y,
+            lal_s2z,
+        ) = SimInspiralTransformPrecessingNewInitialConditions(
+            thetajn[i].item(),
+            phijl[i].item(),
+            theta1[i].item(),
+            theta2[i].item(),
+            phi12[i].item(),
+            chi1[i].item(),
+            chi2[i].item(),
+            mass_1[i].item() * MSUN,
+            mass_2[i].item() * MSUN,
+            f_ref,
+            phi_ref[i].item(),
+        )
+
+        # check if the values are close up to 4 decimal places
+        assert np.isclose(incl[i].item(), lal_incl, atol=1e-4)
+        assert np.isclose(s1x[i].item(), lal_s1x, atol=1e-4)
+        assert np.isclose(s1y[i].item(), lal_s1y, atol=1e-4)
+        assert np.isclose(s1z[i].item(), lal_s1z, atol=1e-4)
+        assert np.isclose(s2x[i].item(), lal_s2x, atol=1e-4)
+        assert np.isclose(s2y[i].item(), lal_s2y, atol=1e-4)
+        assert np.isclose(s2z[i].item(), lal_s2z, atol=1e-4)
