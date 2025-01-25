17b2e6b39e211e8b0cb9a1240dc27e6e032ad04c
Ravi Kumar <ravioli1369@gmail.com>
2025-01-25 18:35:26 +0000

    add phenom tests

--- tests/transforms/test_iirfilter.py
+++ tests/transforms/test_iirfilter.py
@@ -1,9 +1,21 @@
+import lal
+import lalsimulation
 import numpy as np
 import pytest
 import torch
+from astropy import units as u
 from scipy.signal import butter, filtfilt
+from torch.distributions import Uniform
 
+from ml4gw.constants import MSUN
 from ml4gw.transforms.iirfilter import IIRFilter
+from ml4gw.waveforms.conversion import (
+    bilby_spins_to_lalsim,
+    chirp_mass_and_mass_ratio_to_components,
+)
+
+low_cutoff = 100
+high_cutoff = 20
 
 
 @pytest.fixture(params=[256, 512, 1024, 2048])
@@ -16,7 +28,7 @@ def order(request):
     return request.param
 
 
-def test_butterworth(sample_rate, order):
+def test_butterworth_synthetic_signal(sample_rate, order):
     t = np.linspace(0, 1.0, sample_rate, endpoint=False)
     tone_freq = 50
     noise_amplitude = 0.5
@@ -25,8 +37,6 @@ def test_butterworth(sample_rate, order):
     noise = noise_amplitude * np.random.normal(size=t.shape)
     combined_signal = signal + noise
 
-    low_cutoff = 100
-    high_cutoff = 20
     slice_length = int(0.1 * sample_rate)
 
     butterworth_low = IIRFilter(
@@ -96,4 +106,216 @@ def test_butterworth(sample_rate, order):
         scipy_filtered_data_high,
         torch_filtered_data_high[0],
         atol=1e-1,
+    )
+
+
+N_SAMPLES = 1
+
+
+@pytest.fixture()
+def chirp_mass(request):
+    dist = Uniform(5, 100)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def mass_ratio():
+    dist = Uniform(0.125, 0.99)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def a_1(request):
+    dist = Uniform(0, 0.90)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def a_2(request):
+    dist = Uniform(0, 0.90)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def tilt_1(request):
+    dist = Uniform(0, torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def tilt_2(request):
+    dist = Uniform(0, torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def phi_12(request):
+    dist = Uniform(0, 2 * torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def phi_jl(request):
+    dist = Uniform(0, 2 * torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def distance(request):
+    dist = Uniform(100, 3000)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def theta_jn(request):
+    dist = Uniform(0, torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture()
+def phase(request):
+    dist = Uniform(0, 2 * torch.pi)
+    return dist.sample(torch.Size((N_SAMPLES,)))
+
+
+@pytest.fixture(params=[20, 40])
+def f_ref(request):
+    return request.param
+
+
+def test_butterworth_phenom_signal(
+    sample_rate,
+    order,
+    chirp_mass,
+    mass_ratio,
+    distance,
+    phase,
+    f_ref,
+    theta_jn,
+    phi_jl,
+    tilt_1,
+    tilt_2,
+    phi_12,
+    a_1,
+    a_2,
+):
+    mass_1, mass_2 = chirp_mass_and_mass_ratio_to_components(
+        chirp_mass, mass_ratio
+    )
+    (
+        inclination,
+        chi1x,
+        chi1y,
+        chi1z,
+        chi2x,
+        chi2y,
+        chi2z,
+    ) = bilby_spins_to_lalsim(
+        theta_jn,
+        phi_jl,
+        tilt_1,
+        tilt_2,
+        phi_12,
+        a_1,
+        a_2,
+        mass_1,
+        mass_2,
+        f_ref,
+        phase,
+    )
+
+    params = dict(
+        m1=mass_1.item() * MSUN,
+        m2=mass_2.item() * MSUN,
+        S1x=chi1x.item(),
+        S1y=chi1y.item(),
+        S1z=chi1z.item(),
+        S2x=chi2x.item(),
+        S2y=chi2y.item(),
+        S2z=chi2z.item(),
+        distance=(distance.item() * u.Mpc).to("m").value,
+        inclination=inclination.item(),
+        phiRef=phase.item(),
+        longAscNodes=0.0,
+        eccentricity=0.0,
+        meanPerAno=0.0,
+        deltaF=1.0 / sample_rate,
+        f_min=10.0,
+        f_ref=f_ref,
+        f_max=300,
+        approximant=lalsimulation.IMRPhenomPv2,
+        LALpars=lal.CreateDict(),
+    )
+    hp_lal, _ = lalsimulation.SimInspiralChooseFDWaveform(**params)
+    hp_lal = hp_lal.data.data
+
+    slice_length = int(0.1 * sample_rate)
+
+    b, a = butter(
+        order,
+        low_cutoff,
+        btype="low",
+        analog=False,
+        output="ba",
+        fs=sample_rate,
+    )
+    scipy_filtered_data_low = filtfilt(b, a, hp_lal)[
+        slice_length:-slice_length
+    ]
+    b, a = butter(
+        order,
+        high_cutoff,
+        btype="high",
+        analog=False,
+        output="ba",
+        fs=sample_rate,
+    )
+    scipy_filtered_data_high = filtfilt(b, a, hp_lal)[
+        slice_length:-slice_length
+    ]
+
+    butterworth_low = IIRFilter(
+        order,
+        low_cutoff,
+        btype="low",
+        analog=False,
+        fs=sample_rate,
+    )
+    butterworth_high = IIRFilter(
+        order,
+        high_cutoff,
+        btype="high",
+        analog=False,
+        fs=sample_rate,
+    )
+
+    torch_filtered_data_low = butterworth_low(
+        torch.tensor(hp_lal.real).repeat(10, 1)
+    )[:, slice_length:-slice_length].numpy()
+    torch_filtered_data_high = butterworth_high(
+        torch.tensor(hp_lal.real).repeat(10, 1)
+    )[:, slice_length:-slice_length].numpy()
+
+    # test batch processing
+    for i in range(9):
+        assert np.allclose(
+            torch_filtered_data_low[0],
+            torch_filtered_data_low[i + 1],
+            atol=float(np.finfo(float).eps),
+        )
+        assert np.allclose(
+            torch_filtered_data_high[0],
+            torch_filtered_data_high[i + 1],
+            atol=float(np.finfo(float).eps),
+        )
+
+    assert np.allclose(
+        1e21 * scipy_filtered_data_low,
+        1e21 * torch_filtered_data_low[0],
+        atol=1,
+    )
+    assert np.allclose(
+        1e21 * scipy_filtered_data_high,
+        1e21 * torch_filtered_data_high[0],
+        atol=1,
     )
