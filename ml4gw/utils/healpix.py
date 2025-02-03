98b90fc4630e537aead9d4da40e5837c825b8c52
Ravi Kumar <ravioli1369@gmail.com>
2025-02-03 05:11:48 +0000

    refactor types

--- ml4gw/utils/healpix.py
+++ ml4gw/utils/healpix.py
@@ -1,23 +1,24 @@
-from typing import Union
+from typing import Tuple
 
+import numpy as np
 import torch
 
 from ..constants import PI
+from ..types import HealpixIndex
 
 
 def nest2uniq(
-    nside: Union[int, torch.Tensor], ipix: Union[int, torch.Tensor]
-) -> Union[int, torch.Tensor]:
+    nside: HealpixIndex,
+    ipix: HealpixIndex,
+) -> HealpixIndex:
     return 4 * nside * nside + ipix
 
 
-def nside2npix(nside: Union[int, torch.Tensor]) -> Union[int, torch.Tensor]:
+def nside2npix(nside: HealpixIndex) -> HealpixIndex:
     return 12 * nside * nside
 
 
-def nside2pixarea(
-    nside: Union[int, torch.Tensor], degrees: bool = True
-) -> Union[float, torch.Tensor]:
+def nside2pixarea(nside: HealpixIndex, degrees: bool = True) -> HealpixIndex:
     pixarea = 4 * PI / nside2npix(nside)
 
     if degrees:
@@ -27,7 +28,10 @@ def nside2pixarea(
 
 
 def lonlat2thetaphi(
-    lon: Union[int, torch.Tensor], lat: Union[int, torch.Tensor]
-) -> tuple[torch.Tensor, torch.Tensor]:
+    lon: HealpixIndex, lat: HealpixIndex
+) -> Tuple[HealpixIndex, HealpixIndex]:
     lon, lat = torch.asarray(lon), torch.asarray(lat)
-    return PI / 2.0 - torch.deg2rad(lat), torch.deg2rad(lon)
+    if isinstance(lon, torch.Tensor) and isinstance(lat, torch.Tensor):
+        return PI / 2.0 - torch.deg2rad(lat), torch.deg2rad(lon)
+    else:
+        return PI / 2.0 - np.deg2rad(lat), np.deg2rad(lon)
