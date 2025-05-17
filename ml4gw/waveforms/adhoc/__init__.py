8d48732b4f8658af8678498bd16d8b554ade5c2d
Katya Govorkova <katyag@mit.edu>
2025-05-17 02:43:35 -0700

    multi SG by Eric M.

--- ml4gw/waveforms/adhoc/__init__.py
+++ ml4gw/waveforms/adhoc/__init__.py
@@ -1,5 +1,5 @@
 from .ringdown import Ringdown
-from .sine_gaussian import SineGaussian
+from .sine_gaussian import SineGaussian, MultiSineGaussian
 from .gaussian import Gaussian
 from .white_noise_burst import WhiteNoiseBurst
 from .cosmic_string import GenerateString
