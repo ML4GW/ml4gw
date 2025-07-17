e3c94ec4d94c726a7f70b28d6ba750b77057c294
Andy Chen <andy.c.80297@gmail.com>
2025-07-16 18:19:33 -0700

    Add online SnrRescaler

--- ml4gw/transforms/__init__.py
+++ ml4gw/transforms/__init__.py
@@ -1,7 +1,7 @@
 from .pearson import ShiftedPearsonCorrelation
 from .qtransform import QScan, SingleQTransform
 from .scaler import ChannelWiseScaler
-from .snr_rescaler import SnrRescaler
+from .snr_rescaler import SnrRescaler, SnrRescaler_Online
 from .spectral import SpectralDensity
 from .spectrogram import MultiResolutionSpectrogram
 from .spline_interpolation import SplineInterpolate
--- ml4gw/transforms/snr_rescaler.py
+++ ml4gw/transforms/snr_rescaler.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 import torch
 
+from ml4gw import gw
 from ml4gw.gw import compute_network_snr
 from ml4gw.transforms.transform import FittableSpectralTransform
 from ml4gw.types import BatchTensor, TimeSeries2d, WaveformTensor
@@ -73,3 +74,79 @@ class SnrRescaler(FittableSpectralTransform):
         rescaled_responses = responses * weights.view(-1, 1, 1)
 
         return rescaled_responses, target_snrs
+
+class SnrRescaler_Online(torch.nn.Module):
+    """
+    Module that calculates SNRs of injections relative
+    to a given ASD and performs augmentation of the waveform
+    dataset by rescaling injections such that they have SNRs
+    given by `target_snrs`. If this argument is `None`, each
+    injection is randomly matched with and scaled to the SNR
+    of a different injection from the batch.
+    """
+
+    def __init__(
+        self,
+        sample_rate: float,
+        highpass: Optional[float] = None,
+        # lowpass: Optional[float] = None,
+    ) -> None:
+        super().__init__()
+        self.sample_rate = sample_rate
+        self.highpass = highpass
+
+    def forward(
+        self,
+        responses: gw.WaveformTensor,
+        psds: torch.Tensor,
+        target_snrs: Union[BatchTensor, float, None],
+    ) -> gw.WaveformTensor:
+        # we can either specify one PSD for all batch
+        # elements, or a PSD for each batch element
+        if psds.ndim > 2 and len(psds) != len(responses):
+            raise ValueError(
+                "Background PSDs must either be two dimensional "
+                "or have a PSD specified for every element in the "
+                "batch. Expected {}, found {}".format(
+                    len(responses), len(psds)
+                )
+            )
+
+        # interpolate the number of PSD frequency bins down
+        # to the value expected by the shape of the waveforms
+        num_freqs = responses.size(-1) // 2 + 1
+        if psds.size(-1) != num_freqs:
+            if psds.ndim == 2:
+                psds = psds[None]
+                reshape = True
+            else:
+                reshape = False
+
+            psds = torch.nn.functional.interpolate(psds, size=(num_freqs,))
+            if reshape:
+                psds = psds.view(-1, num_freqs)
+
+        # compute the SNRs of the existing signals
+        snrs = compute_network_snr(
+            responses,
+            psds,
+            self.sample_rate,
+            self.highpass,
+        )
+
+        if target_snrs is None:
+            # if we didn't specify any target SNRs, then shuffle
+            # the existing SNRs of the waveforms as they stand
+            idx = torch.randperm(len(snrs))
+            target_snrs = snrs[idx]
+        elif not isinstance(target_snrs, torch.Tensor):
+            # otherwise if we provided just a float, assume
+            # that it's a lower bound on the desired SNR levels
+            target_snrs = snrs.clamp(target_snrs, 1000)
+
+        # reweight the amplitude of the IFO responses
+        # in order to achieve the target SNRs
+        target_snrs.to(snrs.device)
+        weights = target_snrs / snrs
+        return responses * weights.view(-1, 1, 1)
+
