badc87a604cc32ea9a493d332fda3da0cf551e93
Deep Chatterjee <deep.chatterjee@ligo.org>
2026-01-29 10:06:10 -0500

    allow flexible imports

--- ml4gw/__init__.py
+++ ml4gw/__init__.py
@@ -1,5 +1,27 @@
 from importlib.metadata import version
 
+from . import (
+    augmentations,
+    dataloading,
+    distributions,
+    gw,
+    nn,
+    spectral,
+    transforms,
+    utils,
+    waveforms,
+)
 from .constants import *
+
+__all__ = [
+    "augmentations",
+    "dataloading",
+    "distributions",
+    "gw",
+    "nn",
+    "spectral",
+    "transforms",
+    "waveforms",
+]
 
 __version__ = version(__name__)
--- ml4gw/nn/__init__.py
+++ ml4gw/nn/__init__.py
@@ -4,3 +4,5 @@ architecture components. These can be a good place
 to get started, rather than defining your own
 architecture from the start.
 """
+
+from . import autoencoder, norm, resnet, streaming
