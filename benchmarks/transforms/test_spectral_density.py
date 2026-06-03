db85d9be4af3c38bebd77d33c591481b8dc2570c
William Benoit <benoi090@umn.edu>
2026-06-03 10:46:26 -0700

    Set max time and parameterize spectral density by averager

--- benchmarks/transforms/test_spectral_density.py
+++ benchmarks/transforms/test_spectral_density.py
@@ -1,15 +1,24 @@
 """Benchmarks for SpectralDensity."""
 
+import pytest
 import torch
 from constants import KERNEL_LEN, NUM_CHANNELS, SAMPLE_RATE
 
 from ml4gw.transforms import SpectralDensity
 
 
-def test_spectral_density_forward(benchmark, batch_size, device, maybe_sync):
+@pytest.fixture(params=["mean", "median"])
+def average(request):
+    return request.param
+
+
+def test_spectral_density_forward(
+    benchmark, batch_size, average, device, maybe_sync
+):
     spectral_density = SpectralDensity(
         sample_rate=SAMPLE_RATE,
         fftlength=KERNEL_LEN,
+        average=average,
     ).to(device)
     x = torch.randn(batch_size, NUM_CHANNELS, SAMPLE_RATE * 4, device=device)
     benchmark(maybe_sync(spectral_density), x)
